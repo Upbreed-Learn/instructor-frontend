7eb9042ed3662ba1de633107cf1e5a77f32a629d
OGEmanuel <1blacksanta416@gmail.com>
2025-11-01 00:07:34 +0100

    add:Settings page

---
+++ src/assets/jsx-icons/image-upload-icon.tsx
@@ -0,0 +1,28 @@
+const ImageUploadIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5 21C9.21 16.249 13.941 9.948 21 14.673M17 4.5C17.491 3.994 18.8 2 19.5 2M19.5 2C20.2 2 21.509 3.994 22 4.5M19.5 2V10"
+        stroke="#737373"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M21 13C20.998 17.147 20.947 19.27 19.609 20.609C18.217 22 15.979 22 11.5 22C7.022 22 4.782 22 3.391 20.609C2 19.218 2 16.979 2 12.5C2 8.022 2 5.782 3.391 4.391C4.782 3 7.021 3 11.5 3H14"
+        stroke="#737373"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default ImageUploadIcon;
